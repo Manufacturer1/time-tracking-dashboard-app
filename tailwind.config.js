aa02ce4c91190fcc9bd28f952d68a8ab2607edc1
Mihai <Mihai@DESKTOP-3U2EGAU>
2025-01-13 21:45:28 +0200

    started time-tracking-app challenge

---
+++ tailwind.config.js
@@ -0,0 +1,30 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      colors:{
+        primary:{
+          blue:'hsl(246, 80%, 60%)',
+          lightRed:'hsl(15, 100%, 70%)',
+          softBlue:'hsl(195, 74%, 62%)',
+          lightRed:'hsl(348, 100%, 68%)',
+          limeGreen:'hsl(145, 58%, 55%)',
+          violet:'hsl(264, 64%, 52%)',
+          softOrange:'hsl(43, 84%, 65%)'
+        },
+        neutral:{
+          veryDarkBlue:'hsl(226, 43%, 10%)',
+          darkBlue:'hsl(235, 46%, 20%)',
+          dessaturatedBlue:'hsl(235, 45%, 61%)',
+          paleBlue:'hsl(236, 100%, 87%)'
+        }
+      },
+    },
+  },
+  plugins: [],
+}
+
