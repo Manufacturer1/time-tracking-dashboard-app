d8fe39fa6407f2a475ab81124e268ccde5600576
Mihai <Mihai@DESKTOP-3U2EGAU>
2025-01-14 21:33:21 +0200

    updated README file

---
+++ README.md
@@ -0,0 +1,53 @@
+# Frontend Mentor - Time tracking dashboard solution
+
+This is a solution to the [Time tracking dashboard challenge]
+
+## Table of contents
+
+- [Overview](#overview)
+  - [The challenge](#the-challenge)
+  - [Screenshot](#screenshot)
+  - [Links](#links)
+- [My process](#my-process)
+  - [Built with](#built-with)
+  - [What I learned](#what-i-learned)
+- [Author](#author)
+
+## Overview
+
+### The challenge
+
+Users should be able to:
+
+- View the optimal layout for the site depending on their device's screen size
+- See hover states for all interactive elements on the page
+- Switch between viewing Daily, Weekly, and Monthly stats
+
+### Screenshot
+
+![dashboard component screenshot](./screenshots/dashboard-component.png)
+
+### Links
+
+- Solution URL: [Solution Github](hhttps://github.com/Manufacturer1/time-tracking-dashboard-app)
+- Live Site URL: [Vercel Host](https://time-tracking-dashboard-app-eta.vercel.app/)
+
+## My process
+
+### Built with
+
+- Semantic HTML5 markup
+- CSS custom properties
+- CSS Grid
+- Mobile-first workflow
+- [React](https://reactjs.org/) - JS library
+- [Tailwind](https://tailwindui.com/) - React framework
+
+### What I learned
+
+I've learned a lot about how to manage components, how to create resuable components in React and a little bit of css grid.
+Also i'm pretty proud of finding a solution to style the dashboard-card-component wich took me a pretty good amount of time finding an optimal soulution.
+
+## Author
+
+- Website - [Manufacturer1](https://github.com/Manufacturer1/time-tracking-dashboard-app)
--- src/components/dashboardCardComponent.jsx
+++ src/components/dashboardCardComponent.jsx
@@ -1,41 +1,41 @@
 import React from "react";
 
-const DashBoardCard = ({backgroundIcon,type,hrs,time,backgroundColor,iconPosition,timeFrameIndex,cardDataLabels}) =>{
-    
-    const checkNumberOfHours = (hrs) =>{
+const DashBoardCard = ({ backgroundIcon, type, hrs, time, backgroundColor, iconPosition, timeFrameIndex, cardDataLabels }) => {
+
+    const checkNumberOfHours = (hrs) => {
         let hours = `${hrs}hrs`;
-        (hrs > 1) ? 
-        hours = `${hrs}hrs` :
-        hours = `${hrs}hr`;
-        
+        (hrs > 1) ?
+            hours = `${hrs}hrs` :
+            hours = `${hrs}hr`;
+
         return hours;
     }
 
-    return(
+    return (
         <div className="relative rounded-xl flex flex-col">
             <div className={`${backgroundColor} rounded-t-xl pr-3 h-14 overflow-hidden mb-[-1em]`}>
-                <img className={`relative ${iconPosition} block ml-auto w-18`} src={backgroundIcon} alt="icon work"/>
+                <img className={`relative ${iconPosition} block ml-auto w-18`} src={backgroundIcon} alt="icon work" />
             </div>
             <div className="w-full z-10 p-5 rounded-xl basis-[80%] cursor-pointer bg-neutral-darkBlue hover:bg-neutral-darkBlueLighter transition-bg duration-300">
                 <div className="flex justify-between items-center mb-4">
                     <h4 className="text-base text-white">{type}</h4>
                     <div className="group">
-                    <svg
-                        width="21"
-                        height="5"
-                        xmlns="http://www.w3.org/2000/svg"
-                        className="fill-[#BBC0FF] group-hover:fill-white transition-colors duration-300"
-                            >
-                        <path d="M2.5 0a2.5 2.5 0 1 1 0 5 2.5 2.5 0 0 1 0-5Zm8 0a2.5 2.5 0 1 1 0 5 2.5 2.5 0 0 1 0-5Zm8 0a2.5 2.5 0 1 1 0 5 2.5 2.5 0 0 1 0-5Z" />
-                    </svg>
-                </div>
+                        <svg
+                            width="21"
+                            height="5"
+                            xmlns="http://www.w3.org/2000/svg"
+                            className="fill-[#BBC0FF] group-hover:fill-white transition-colors duration-300"
+                        >
+                            <path d="M2.5 0a2.5 2.5 0 1 1 0 5 2.5 2.5 0 0 1 0-5Zm8 0a2.5 2.5 0 1 1 0 5 2.5 2.5 0 0 1 0-5Zm8 0a2.5 2.5 0 1 1 0 5 2.5 2.5 0 0 1 0-5Z" />
+                        </svg>
+                    </div>
                 </div>
                 <div className="flex items-center justify-between md:block">
                     <h1 className="text-4xl md:text-5xl font-extralight mb-2">  {
                         checkNumberOfHours(hrs)
                     }
                     </h1>
-                    <p className="text-sm md:text-xs text-neutral-dessaturatedBlue">Last {cardDataLabels[timeFrameIndex]} - 
+                    <p className="text-sm md:text-xs text-neutral-dessaturatedBlue">Last {cardDataLabels[timeFrameIndex]} -
                         {checkNumberOfHours(time)}</p>
                 </div>
             </div>
